74f709834d4bc71d85ecab163fb787b758ab0314
Naimur Rahman <naimur.rahman900@gmail.com>
2024-01-07 01:20:12 +0600

    Some issue resolved

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
-import bkash from "./bkash";
+import bkash from "./Bkash";
 
 const page = () => {
   const searchParams = useSearchParams();
