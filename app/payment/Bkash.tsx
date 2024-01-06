7bc237ae1fc01858454f60da7ac9b87512547b49
Naimur Rahman <naimur.rahman900@gmail.com>
2024-01-07 00:51:13 +0600

    bkashPGW runs perfectly

--- app/payment/Bkash.tsx
+++ app/payment/Bkash.tsx
@@ -1,10 +1,6 @@
 import axiosPost from "./post";
 
-const Bkash = async () => {
-  //   const [msg, setMsg] = useState("");
-  //   const [paymentID, setPaymentID] = useState("");
-  //   const [status, setStatus] = useState("");
-
+const bkash = async (amount: any) => {
   const createPayment = async (amount: any) => {
     const baseURL = "http://localhost:7000";
     const payload = { amount: amount };
@@ -19,7 +15,6 @@ const Bkash = async () => {
     const payload = { paymentID };
     const headers = {};
     const res = await axiosPost(baseURL + "/executePayment", payload, headers);
-    console.log(res);
     return res;
   };
 
@@ -28,11 +23,10 @@ const Bkash = async () => {
     const payload = { paymentID };
     const headers = {};
     const res = await axiosPost(baseURL + "/queryPayment", payload, headers);
-    console.log(res);
     return res;
   };
 
-  const createPaymentRes = await createPayment("200");
+  const createPaymentRes = await createPayment(amount);
   window.location.replace(createPaymentRes.bkashURL);
   console.log(createPaymentRes);
   if (createPaymentRes.statusMessage === "Successful") {
@@ -42,25 +36,6 @@ const Bkash = async () => {
     const queryPaymentRes = await queryPayment(createPaymentRes.paymentID);
     console.log(queryPaymentRes);
   }
-
-  //   setPaymentID(createPaymentRes.paymentID);
-  //   setStatus(createPaymentRes.status);
-  // window.location.replace(createPaymentRes.bkashURL);
-
-  //   if (paymentID !== "") {
-  //     if (status === "success") {
-  //       const executePaymentRes = await executePayment(paymentID);
-  //       console.log(executePaymentRes);
-  //       setMsg(executePaymentRes.statusMessage);
-  //     } else {
-  //       const data = await queryPayment(paymentID);
-  //       console.log(data);
-  //       setMsg(status);
-  //     }
-  //   }
-
-  //   return <div>{msg != "" ? <h1>{msg}</h1> : <h1>Nothing happend</h1>}</div>;
-  return <div>Jodd</div>;
 };
 
-export default Bkash;
+export default bkash;
